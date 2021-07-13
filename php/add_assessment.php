9d77a4de104753a27da9fc40717161a36ad5fadb
Namra Habib <namiihabib987@gmail.com>
2021-07-13 13:52:24 -0700

    done with assessments part

--- php/add_assessment.php
+++ php/add_assessment.php
@@ -4,11 +4,11 @@ include 'DBManager.php';
 
 if(isset($_POST['submit'])){
 
-    $studentId = (int)$_POST['studentId'];
+    $studentId = $_POST['studentId'];
     $assessmentName = $_POST['assessmentName'];
     $subjectName = $_POST['subjectName'];
-    $totalMarks = (float)$_POST['totalMarks'];
-    $obtainedMarks = (float)$_POST['obtainedMarks'];
+    $totalMarks = $_POST['totalMarks'];
+    $obtainedMarks = $_POST['obtainedMarks'];
 
     echo var_dump($studentId);
     echo var_dump($assessmentName);
@@ -16,12 +16,11 @@ if(isset($_POST['submit'])){
     echo var_dump($totalMarks);
     echo var_dump($obtainedMarks);
 
-    $query = "INSERT INTO Assessments(student_id,assessment_name,subject_title,total_marks,obtained_marks) VALUES((int)$studentId,'$assessmentName','$subjectName',(float)$totalMarks,(float)$obtainedMarks)";
+    $query = "INSERT INTO Assessments(student_id,assessment_name,subject_title,total_marks,obtained_marks) VALUES($studentId,'$assessmentName','$subjectName',$totalMarks,$obtainedMarks)";
     $result = mysqli_query($connection,$query);
-    echo $result;
     if($result){
-        echo "<script>alert('Assessment inserted successfully!')</script>";
-        header("Location: student_assessments.php");
+        echo '<script>confirm("Student Assessment added successfully")</script>';
+        ?><script>location.replace("student_assessments.php")</script><?php
     }else{
         echo "<script>alert('Error! Assessment not inserted')</script>";
     }
